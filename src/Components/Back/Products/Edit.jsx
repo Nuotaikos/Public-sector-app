7d39abd06c76f99b9e386a560b19362b31c7b2e5
Diana <oganauskiene@gmail.com>
2022-08-03 10:58:24 +0300

    modal div updated

--- src/Components/Back/Products/Edit.jsx
+++ src/Components/Back/Products/Edit.jsx
@@ -35,46 +35,52 @@ function Edit() {
   }
 
   return (
-    <div className="card mt-4">
-      <div className="card-header">
-        <h2>Update Sector</h2>
-      </div>
-      <div className="card-body">
-        <div className="form-group">
-          <label className="form-group mb-2">Sectors</label>
-          <select className="form-control" onChange={e => setTitle(e.target.value)} value={title}>
-            <option value="0" >Please, select Sector</option>
-            <option value="Aplinkosauga">Aplinkosauga</option>
-            <option value="Civilinė sauga">Civilinė sauga</option>
-            <option value="Įdarbinimas">Įdarbinimo paslaugos</option>
-            <option value="Kultūra">Kultūra</option>
-            <option value="Medicina">Medicina</option>
-            <option value="Mokesčių administravimas">Mokesčių administravimas</option>
-            <option value="Mokslo">Mokslo paslaugos</option>
-            <option value="Priešgaisrinė apsauga">Priešgaisrinė apsauga</option>
-            <option value="Socialinės paslaugos">Socialinės paslaugos</option>
-            <option value="Sportas">Sportas</option>
-            <option value="Susiekimas">Susiekimas</option>
-            <option value="Švietimas">Švietimas</option>
-            <option value="Teisėsauga">Teisėsauga</option>
-          </select>
-        </div>
-        <div className="form-group">
-          <label>Municipalities</label>
-          <select className="form-control" onChange={e => setCat(e.target.value)} value={cat}>
-            <option value="0">Please, select Municipality</option>
-            {
-              cats ? cats.map(c => <option key={c.id} value={c.id}>{c.title}</option>) : null
-            }
-          </select>
-          <div className="modal-footer">
-            <button type="button" className="btn btn-secondary" onClick={() => setModalProduct(null)}>Close</button>
-            <button type="button" className="btn btn-primary" onClick={handleEdit}>Save changes</button>
+    <div className="modal">
+      <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">Update Sector</h5>
+            <button type="button" className="close" onClick={() => setModalProduct(null)}>
+              <span>&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">
+            <div className="form-group">
+              <label className="form-group mb-2">Sectors</label>
+              <select className="form-control" onChange={e => setTitle(e.target.value)} value={title}>
+                <option value="0" >Please, select Sector</option>
+                <option value="Aplinkosauga">Aplinkosauga</option>
+                <option value="Civilinė sauga">Civilinė sauga</option>
+                <option value="Įdarbinimas">Įdarbinimo paslaugos</option>
+                <option value="Kultūra">Kultūra</option>
+                <option value="Medicina">Medicina</option>
+                <option value="Mokesčių administravimas">Mokesčių administravimas</option>
+                <option value="Mokslo">Mokslo paslaugos</option>
+                <option value="Priešgaisrinė apsauga">Priešgaisrinė apsauga</option>
+                <option value="Socialinės paslaugos">Socialinės paslaugos</option>
+                <option value="Sportas">Sportas</option>
+                <option value="Susiekimas">Susiekimas</option>
+                <option value="Švietimas">Švietimas</option>
+                <option value="Teisėsauga">Teisėsauga</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <label>Municipalities</label>
+              <select className="form-control" onChange={e => setCat(e.target.value)} value={cat}>
+                <option value="0">Please, select Municipality</option>
+                {
+                  cats ? cats.map(c => <option key={c.id} value={c.id}>{c.title}</option>) : null
+                }
+              </select>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" onClick={() => setModalProduct(null)}>Close</button>
+                <button type="button" className="btn btn-primary" onClick={handleEdit}>Save changes</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
 }
-
 export default Edit;
